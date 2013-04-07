3fbd3f32b207a687247f52bd74e753f151660a61
Ahmad Luqman <ahmad.luqman@gmail.com>
2013-04-08 00:02:16 +0500

    WIP changes to Dancing with googlers

--- CJ2012QR-DancingWithTheGooglers-cs/Program.cs
+++ CJ2012QR-DancingWithTheGooglers-cs/Program.cs
@@ -15,10 +15,19 @@ namespace CJ2012QR_DancingWithTheGooglers_cs
 
             for (int t = 0; t < T; t++)
             {
-                var S = Console.ReadLine();
+                var str = Console.ReadLine();
 
-                string G = S;
-                var output = "Case #{0}:" + G;
+                string[] sArray = str.Split(null);
+                int N = Convert.ToInt32(sArray[0]);
+                int S = Convert.ToInt32(sArray[1]);
+                int p = Convert.ToInt32(sArray[2]);
+
+                int []ti = new int[N];
+                for (int i = 0; i < N; i++)
+                    ti[i] = Convert.ToInt32(sArray[i + 3]);
+
+                string y = str;
+                var output = "Case #{0}:" + y;
                 Console.WriteLine(output, t + 1);
             }
         }
