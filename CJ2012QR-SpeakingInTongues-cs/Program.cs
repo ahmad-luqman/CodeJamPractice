dde796ae6507553c9b4fae5103a6543f5e3ef4ed
Ahmad Luqman <ahmad.luqman@gmail.com>
2013-04-07 21:35:48 +0500

    Solved Speaking In Tongues in C#

--- CJ2012QR-SpeakingInTongues-cs/Program.cs
+++ CJ2012QR-SpeakingInTongues-cs/Program.cs
@@ -12,13 +12,44 @@ namespace CJ2012QR_SpeakingInTongues_cs
         {
             string input = Console.ReadLine();
             int T = Convert.ToInt32(input);
+            
+            var charDictionary = new Dictionary<char, char>();
+            string []mapInput = new string[3];
+            string []mapOutput = new string[3];
+            mapInput[0] = "ejp mysljylc kd kxveddknmc re jsicpdrysi";
+            mapInput[1] ="rbcpc ypc rtcsra dkh wyfrepkym veddknkmkrkcd";
+            mapInput[2] = "de kr kd eoya kw aej tysr re ujdr lkgc jv";
+            mapOutput[0] = "our language is impossible to understand";
+            mapOutput[1] ="there are twenty six factorial possibilities";
+            mapOutput[2] = "so it is okay if you want to just give up";
+            for (int i = 0; i < mapInput.Length; i++)
+            {
+                for (int j = 0; j < mapInput[i].Length; j++)
+                {
+                    char c = mapInput[i][j];
+                    if (!charDictionary.ContainsKey(c))
+                    {
+                        charDictionary[c] = mapOutput[i][j];
+                    }
+                }
+            }
+            charDictionary['z'] = 'q';
+            charDictionary['q'] = 'z';
 
             for (int t = 0; t < T; t++)
             {
                 var S = Console.ReadLine();
 
-                string G = S;
-                var output = "Case #{0}:" + G;
+                string G = string.Empty;
+                foreach (var c in S)
+                {
+                    if (charDictionary.ContainsKey(c))
+                        G += charDictionary[c];
+                    else
+                        G += "#";
+                }
+
+                var output = "Case #{0}: " + G;
                 Console.WriteLine(output, t+1);
             }
         }
