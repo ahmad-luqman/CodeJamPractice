17eda8a8b64158d199dde3cc415b0565f858db97
Ahmad Luqman <ahmad.luqman@gmail.com>
2013-04-07 20:39:33 +0500

    Initial checkin

---
+++ CJ2012QR-SpeakingInTongues-cs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CJ2012QR_SpeakingInTongues_cs
+{
+    class App
+    {
+        public void Run()
+        {
+            string input = Console.ReadLine();
+            int T = Convert.ToInt32(input);
+
+            for (int t = 0; t < T; t++)
+            {
+                var S = Console.ReadLine();
+
+                string G = S;
+                var output = "Case #{0}:" + G;
+                Console.WriteLine(output, t+1);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new App();
+            app.Run();
+        }
+    }
+}
