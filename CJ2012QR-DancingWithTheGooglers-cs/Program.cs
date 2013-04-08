80f72072f60c540da1ab66d3b9087890ad4e146e
Ahmad Luqman <ahmad.luqman@gmail.com>
2013-04-08 22:33:44 +0500

    Solved Dancing With the Googlers

--- CJ2012QR-DancingWithTheGooglers-cs/Program.cs
+++ CJ2012QR-DancingWithTheGooglers-cs/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,11 +24,20 @@ namespace CJ2012QR_DancingWithTheGooglers_cs
                 int p = Convert.ToInt32(sArray[2]);
 
                 int []ti = new int[N];
+                int nonSCount = 0;
+                int SCount = 0;
                 for (int i = 0; i < N; i++)
+                {
                     ti[i] = Convert.ToInt32(sArray[i + 3]);
+                    int tAtI = ti[i];
 
-                string y = str;
-                var output = "Case #{0}:" + y;
+                    if ((p * 3 - 2) <= tAtI) nonSCount++;
+                    else if (1 > tAtI && tAtI < 29) continue;
+                    else if ((p * 3 - 4) <= tAtI) SCount++;
+                }
+
+                int y = nonSCount + Math.Min(SCount, S);
+                var output = "Case #{0}: " + y;
                 Console.WriteLine(output, t + 1);
             }
         }
@@ -38,7 +48,15 @@ namespace CJ2012QR_DancingWithTheGooglers_cs
         static void Main(string[] args)
         {
             var app = new App();
-            app.Run();
+
+            //using (StreamWriter writer = new StreamWriter("C:\\out.txt"))
+            using (StreamReader reader = new StreamReader(@"B-large-practice.in"))
+            {
+                //Console.SetOut(writer);
+                Console.SetIn (reader);
+                app.Run();
+            }
+            
         }
     }
 
