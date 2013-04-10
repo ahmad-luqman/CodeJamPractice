7871862ba987a7654c83d913aaf3b3c42447a881
Ahmad Luqman <ahmad.luqman@gmail.com>
2013-04-10 17:58:11 +0500

    WIP changes related to Recycled Numbers

--- CJ2012QR-RecycledNumbers-cs/Program.cs
+++ CJ2012QR-RecycledNumbers-cs/Program.cs
@@ -9,6 +9,23 @@ namespace CJ2012QR_RecycledNumbers_cs
 {
     class App
     {
+        public string[] RecycledPairs(string original)
+        {
+            //string[] recycledPairs = new string[original.Length];
+            IList<string> recycledPairs = new List<string>();
+            for (int i = 1; i < original.Length; i++)
+            {
+                //if (i == 0)
+                //    continue; //recycledPairs[i] = original;
+                //else
+                string possiblePair = original.Substring(i) + original.Substring(0, i);
+                if(Convert.ToInt32(possiblePair) > Convert.ToInt32(original))
+                    recycledPairs.Add(possiblePair);
+
+            }
+            return recycledPairs.ToArray<string>();
+        }
+
         public void Run()
         {
             string input = Console.ReadLine();
@@ -17,8 +34,25 @@ namespace CJ2012QR_RecycledNumbers_cs
             for (int t = 0; t < T; t++)
             {
                 var str = Console.ReadLine();
+                string[] sArray = str.Split(null);
 
-                var output = "Case #{0}: ";// +y;
+                string sA = sArray[0];
+                string sB = sArray[1];
+                int A = Convert.ToInt32(sA);
+                int B = Convert.ToInt32(sB);
+
+                int y = 0;
+                for (int i = A; i < B; i++)
+                {
+                    string[] pairs = RecycledPairs(i.ToString());
+                    foreach (var pair in pairs)
+                    {
+                        if (Convert.ToInt32(pair) <= B)
+                            y++;
+                    }
+                }
+
+                var output = "Case #{0}: " + y;
                 Console.WriteLine(output, t + 1);
             }
         }
@@ -30,11 +64,11 @@ namespace CJ2012QR_RecycledNumbers_cs
         {
             var app = new App();
 
-            using (StreamWriter writer = new StreamWriter("C:\\out.txt"))
-            using (StreamReader reader = new StreamReader(@"B-large-practice.in"))
+            //using (StreamWriter writer = new StreamWriter("C:\\out.txt"))
+            //using (StreamReader reader = new StreamReader(@"B-large-practice.in"))
             {
-                Console.SetOut(writer);
-                Console.SetIn(reader);
+                //Console.SetOut(writer);
+                //Console.SetIn(reader);
                 app.Run();
             }
 
