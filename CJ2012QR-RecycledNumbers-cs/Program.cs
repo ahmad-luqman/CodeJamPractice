c9c2eae6f5e572bd12289ffae3a66357a2011fb1
Ahmad Luqman <ahmad.luqman@gmail.com>
2013-04-10 16:53:34 +0500

    Initial CheckIn for Recycled Numbers

---
+++ CJ2012QR-RecycledNumbers-cs/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CJ2012QR_RecycledNumbers_cs
+{
+    class Program
+    {
+        class App
+        {
+            public void Run()
+            {
+                string input = Console.ReadLine();
+                int T = Convert.ToInt32(input);
+
+                for (int t = 0; t < T; t++)
+                {
+                    var str = Console.ReadLine();
+
+                    var output = "Case #{0}: ";// +y;
+                    Console.WriteLine(output, t + 1);
+                }
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                var app = new App();
+
+                //using (StreamWriter writer = new StreamWriter("C:\\out.txt"))
+                //using (StreamReader reader = new StreamReader(@"B-large-practice.in"))
+                {
+                    //Console.SetOut(writer);
+                    //Console.SetIn(reader);
+                    app.Run();
+                }
+
+            }
+        }
+    }
+}
