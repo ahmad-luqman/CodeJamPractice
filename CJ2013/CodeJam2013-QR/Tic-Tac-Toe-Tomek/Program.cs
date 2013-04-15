247115187260d9bfb77079e570119fdd1a891cbd
Ahmad Luqman <ahmad.luqman@gmail.com>
2013-04-15 16:57:38 +0500

    CJ2013 blank projects checkin

---
+++ CJ2013/CodeJam2013-QR/Tic-Tac-Toe-Tomek/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_Tac_Toe_Tomek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
