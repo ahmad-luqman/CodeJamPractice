01fafc6d020128ee30ea68648cd4145dc8852072
Ahmad Luqman <ahmad.luqman@gmail.com>
2013-04-15 17:01:23 +0500

    Add two more projects

---
+++ CJ2013/CodeJam2013-QR/Fair-and-Square/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fair_and_Square
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
